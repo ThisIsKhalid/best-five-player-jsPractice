c409f350b125d51b732a65b65c44933d6bdaadd4
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-08-23 12:26:03 +0600

    project done

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,8 @@
 const selectedPlayersArray = [];
 
 function addPlayer(players){
-    // console.log(players.parentNode.parentNode.children[0].innerText);
     const playerName = players.parentNode.parentNode.children[0].innerText;
-    // console.log(playerName);
     selectedPlayersArray.push(playerName);
-
-    // const selectedPlayers = document.getElementById('selected-players');
-    // const playerNameList = document.createElement('li');
-    // playerNameList.innerText = playerName;
-    // selectedPlayers.appendChild(playerNameList);
 
     if(selectedPlayersArray.length <= 5){
         const selectedPlayers = document.getElementById('selected-players');
@@ -30,7 +23,7 @@ function getInputValueById(inputId){
     const inputValueString = inputValue.value;
     const totalInputValue = parseFloat(inputValueString);
     if(isNaN(totalInputValue)){
-        alert('Please enter a valid value!')
+        alert('Please enter a valid value!');
     }
     else {
         return totalInputValue;
@@ -57,16 +50,10 @@ function setTextFieldById(textId, value){
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const playerNumber = selectedPlayersArray.length;
-    // const perPlayer = document.getElementById('per-player');
-    // const perPlayerString = perPlayer.value;
-    // const perPlayerTotal = parseFloat(perPlayerString);
     const perPlayerTotal = getInputValueById('per-player');
 
     const playersExpenses = playerNumber * perPlayerTotal;
     setTextFieldById('players-expenses', playersExpenses);
-    console.log(typeof playersExpenses);
-    
-    // const playerExpenses = getTextFieldById('player-expenses');
 
 })
 
